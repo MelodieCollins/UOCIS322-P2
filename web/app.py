533cff017ecf38bca65e473c2e36b6ba2cb46d1c
Melodie Collins <mcolli11@testium.cs.uoregon.edu>
2021-04-25 19:41:01 -0700

    Completed Project 2 with file checking.

--- web/app.py
+++ web/app.py
@@ -1,10 +1,42 @@
 """
-John Doe's Flask API.
+Melodie Collins' Flask API.
 """
 
-from flask import Flask
+from flask import Flask, abort, render_template
+import os
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('404.html')
+
+@app.errorhandler(403)
+def error_403(e):
+    return render_template('403.html')
+
+@app.route("/<path:f_name>")
+def respond(f_name):
+    '''
+    If page starts with (~ // ..) respond with 403 forbidden error.
+    '''
+    if (("~" in f_name) or ("//" in f_name) or (".." in f_name)):
+        abort(403)
+
+    '''If path/to/name.html is in document path (from DOCROOT), send contents of name.html
+    or name.css with proper http response. Else, respond with 404 not found error.
+    '''
+    if (f_name[-5:] == ".html" or f_name[-4:] == ".css"):
+        '''Make list of names of files and directories in current working directory. If
+        name of file in list, open and read it.
+        '''
+        if f_name in os.listdir(os.curdir):
+            f=open(f_name, 'r')
+            return f.read()
+        else:
+            abort(404)
+    else:
+        return "Invalid input!\n"
 
 @app.route("/")
 def hello():
